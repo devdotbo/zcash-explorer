a59d1a7df66ec871ab7bba6b498e0181e2e6755d
devdotbo <7118.eth@proton.me>
2025-12-31 09:03:59 +0000

    Enrich transaction inputs with address and value from previous outputs
    Zebra/Zaino does not include address and value fields in vin entries
    like zcashd does. This caused transaction inputs to display with empty
    address and value on detail pages.
    
    Add enrich_vin_data/1 function that looks up each input's previous
    transaction and extracts address/value from the corresponding output.

--- lib/zcash_explorer_web/controllers/transaction_controller.ex
+++ lib/zcash_explorer_web/controllers/transaction_controller.ex
@@ -5,7 +5,8 @@ defmodule ZcashExplorerWeb.TransactionController do
     case Zcashex.getrawtransaction(txid, 1) do
       {:ok, tx} ->
         tx_data = Zcashex.Transaction.from_map(tx)
-        render(conn, "tx.html", tx: tx_data, page_title: "Zcash Transaction #{txid}")
+        tx_with_enriched_vin = enrich_vin_data(tx_data)
+        render(conn, "tx.html", tx: tx_with_enriched_vin, page_title: "Zcash Transaction #{txid}")
 
       {:error, _reason} ->
         conn
@@ -31,5 +32,40 @@ defmodule ZcashExplorerWeb.TransactionController do
         |> put_resp_content_type("application/json")
         |> send_resp(404, Poison.encode!(error))
     end
+  end
+
+  # Enrich vin entries with address and value from previous transaction outputs
+  defp enrich_vin_data(tx) do
+    enriched_vin = Enum.map(tx.vin, fn vin ->
+      # Skip coinbase transactions (they have no previous tx to look up)
+      if vin.coinbase != nil or vin.txid == nil do
+        vin
+      else
+        case Zcashex.getrawtransaction(vin.txid, 1) do
+          {:ok, prev_tx} ->
+            prev_tx_data = Zcashex.Transaction.from_map(prev_tx)
+            # Get the specific output being spent
+            prev_vout = Enum.find(prev_tx_data.vout, fn vout -> vout.n == vin.vout end)
+
+            if prev_vout do
+              # Extract address from scriptPubKey.addresses
+              address = case prev_vout.scriptPubKey do
+                nil -> nil
+                spk -> List.first(spk.addresses || [])
+              end
+
+              # Update the vin struct with address and value
+              %{vin | address: address, value: prev_vout.value}
+            else
+              vin
+            end
+
+          {:error, _} ->
+            vin
+        end
+      end
+    end)
+
+    %{tx | vin: enriched_vin}
   end
 end
